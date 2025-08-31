f82ffa3d3aa5a383d8ef52c7aa653781de3c4f50
fr4ncode <150541037+fr4ncode@users.noreply.github.com>
2025-08-30 22:52:19 -0500

    Update CategoryController.java

--- src/main/java/dev/francode/ordersystem/controller/CategoryController.java
+++ src/main/java/dev/francode/ordersystem/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/categories")
+@RequestMapping("/api/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
